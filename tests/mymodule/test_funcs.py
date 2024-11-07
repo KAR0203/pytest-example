61af4543c26643f255cc61a09e5be02eef1f76c5
KAR0203 <pandharekaran02@gmail.com>
2024-11-07 10:52:38 +0530

    chaged

--- tests/mymodule/test_funcs.py
+++ tests/mymodule/test_funcs.py
@@ -19,3 +19,9 @@ def test_multiply():
 @pytest.mark.difficult_operation
 def test_divide():
     assert divide(56, 8) == 7
+
+@pytest.mark.skip(reason="Skipping this test due to a bug.")
+def test_another_skipped_case():
+
+        # This is another test case that will be skipped
+    assert subtract(10, 5) == 5  # This assertion will not run
