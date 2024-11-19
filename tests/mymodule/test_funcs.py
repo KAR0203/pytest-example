d4797d0853b5d3f8654066ae32fc407a7e28d914
KAR0203 <pandharekaran02@gmail.com>
2024-11-19 12:17:14 +0530

    changed

--- tests/mymodule/test_funcs.py
+++ tests/mymodule/test_funcs.py
@@ -10,11 +10,11 @@ def test_add():
 
 @pytest.mark.easy_operation
 def test_subtract():
-    assert subtract(3, 6) == 4
+    assert subtract(3, 6) ==-3
 
 @pytest.mark.difficult_operation
 def test_multiply():
-    assert multiply(4, 5) == 21
+    assert multiply(4, 5) == 20
 
 @pytest.mark.difficult_operation
 def test_divide():
